e02e9cbae22e54040f015b92c91201edf6655ef1
Hamza HERBOU <hamza95herbou@gmail.com>
2021-09-22 21:00:43 +0100

    fix diff screens issue

--- Assets/Scripts/SettingsMenuItem.cs
+++ Assets/Scripts/SettingsMenuItem.cs
@@ -1,43 +1,39 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine ;
+using UnityEngine.UI ;
 
-public class SettingsMenuItem : MonoBehaviour
-{
-	[HideInInspector] public Image img;
-	[HideInInspector] public Transform trans;
+public class SettingsMenuItem : MonoBehaviour {
+   [HideInInspector] public Image img ;
+   [HideInInspector] public RectTransform rectTrans ;
 
-	//SettingsMenu reference
-	SettingsMenu settingsMenu;
+   //SettingsMenu reference
+   SettingsMenu settingsMenu ;
 
-	//item button
-	Button button;
+   //item button
+   Button button ;
 
-	//index of the item in the hierarchy
-	int index;
+   //index of the item in the hierarchy
+   int index ;
 
-	void Awake ()
-	{
-		img = GetComponent<Image> ();
-		trans = transform;
+   void Awake () {
+      img = GetComponent<Image> () ;
+      rectTrans = GetComponent<RectTransform> () ;
 
-		settingsMenu = trans.parent.GetComponent <SettingsMenu> ();
+      settingsMenu = rectTrans.parent.GetComponent <SettingsMenu> () ;
 
-		//-1 to ignore the main button
-		index = trans.GetSiblingIndex () - 1;
+      //-1 to ignore the main button
+      index = rectTrans.GetSiblingIndex () - 1 ;
 
-		//add click listener
-		button = GetComponent<Button> ();
-		button.onClick.AddListener (OnItemClick);
-	}
+      //add click listener
+      button = GetComponent<Button> () ;
+      button.onClick.AddListener (OnItemClick) ;
+   }
 
-	void OnItemClick ()
-	{
-		settingsMenu.OnItemClick (index);
-	}
+   void OnItemClick () {
+      settingsMenu.OnItemClick (index) ;
+   }
 
-	void OnDestroy ()
-	{
-		//remove click listener to avoid memory leaks
-		button.onClick.RemoveListener (OnItemClick);
-	}
+   void OnDestroy () {
+      //remove click listener to avoid memory leaks
+      button.onClick.RemoveListener (OnItemClick) ;
+   }
 }
